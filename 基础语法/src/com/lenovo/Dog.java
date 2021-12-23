b27f676bc3c7fa5e58a968dfe9b81ca7c8288d2e
rogerhu618 <roger12863>
2021-12-23 11:47:15 +0800

    first commit for add project to git hub

---
+++ 基础语法/src/com/lenovo/Dog.java
@@ -0,0 +1,61 @@
+package com.lenovo;
+
+import java.util.Objects;
+
+public class Dog {
+    private String name;
+    private int age;
+    private char sex;
+
+    public Dog(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return age == dog.age && Objects.equals(name, dog.name) && Objects.equals(sex, dog.sex);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, sex);
+    }
+}
